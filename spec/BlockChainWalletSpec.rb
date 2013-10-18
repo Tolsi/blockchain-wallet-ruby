44389161b76f9ecb20958ebe3ed4a3df55644ab7
Sergey Tolmachev <tolsi.ru@gmail.com>
2013-10-19 00:54:36 +0400

    test spec fix

--- spec/BlockChainWalletSpec.rb
+++ spec/BlockChainWalletSpec.rb
@@ -8,8 +8,8 @@ describe BlockchainWallet, ".addresses" do
   end
 end
 
-describe BlockchainWallet, "#addresses" do
-  it "returns all addresses" do
+describe BlockchainWallet, ".address_balance" do
+  it "should return '1JX7FrBm4Y96Z8BN6m9fWLN3bWVrEZ7dVs' balance >=0" do
     wallet = BlockchainWallet::Wallet.new("b14d36af-2fa6-4d76-91b5-ff70b8370ec7", "n$grJ74hAshfggnb6864%j78@#^hChttp://ya.ru@contentpurl", nil)
     address_balance=wallet.address_balance("1JX7FrBm4Y96Z8BN6m9fWLN3bWVrEZ7dVs")
     address_balance.balance.should >=(0)
