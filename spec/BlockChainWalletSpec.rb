f1d52bd9549cfb32e68176d8d4599b0278dd236a
Sergey Tolmachev <tolsi.ru@gmail.com>
2013-10-19 16:00:03 +0400

    Some test optimisations
    before and after every test was added

--- spec/BlockChainWalletSpec.rb
+++ spec/BlockChainWalletSpec.rb
@@ -1,17 +1,35 @@
 require File.expand_path("../../lib/blockchain/wallet", __FILE__)
 
-describe Blockchain, ".addresses" do
-  it "returns all addresses" do
-    wallet = Blockchain::Wallet.new("b14d36af-2fa6-4d76-91b5-ff70b8370ec7", "n$grJ74hAshfggnb6864%j78@#^hChttp://ya.ru@contentpurl", nil)
-    addresses=wallet.addresses
+describe Blockchain::Wallet, ".addresses" do
+
+  before do
+    @wallet = Blockchain::Wallet.new("b14d36af-2fa6-4d76-91b5-ff70b8370ec7", "n$grJ74hAshfggnb6864%j78@#^hChttp://ya.ru@contentpurl", nil)
+  end
+
+  it "should returns all addresses" do
+    addresses=@wallet.addresses
     addresses.should_not be_empty
   end
+
+  after do
+    @account = nil
+  end
+
 end
 
-describe Blockchain, ".address_balance" do
+describe Blockchain::Wallet, ".address_balance" do
+
+  before do
+    @wallet = Blockchain::Wallet.new("b14d36af-2fa6-4d76-91b5-ff70b8370ec7", "n$grJ74hAshfggnb6864%j78@#^hChttp://ya.ru@contentpurl", nil)
+  end
+
   it "should return '1JX7FrBm4Y96Z8BN6m9fWLN3bWVrEZ7dVs' balance >=0" do
-    wallet = Blockchain::Wallet.new("b14d36af-2fa6-4d76-91b5-ff70b8370ec7", "n$grJ74hAshfggnb6864%j78@#^hChttp://ya.ru@contentpurl", nil)
-    address_balance=wallet.address_balance("1JX7FrBm4Y96Z8BN6m9fWLN3bWVrEZ7dVs")
+    address_balance=@wallet.address_balance("1JX7FrBm4Y96Z8BN6m9fWLN3bWVrEZ7dVs")
     address_balance.balance.should >=(0)
   end
+
+  after do
+    @account = nil
+  end
+
 end
